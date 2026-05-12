bb1cb635c5e1f89b10452edb6e4ff7742290566e
marigona-ilj <iljazmar@students.zhaw.ch>
2026-05-12 22:45:08 +0200

    change svelte.config.js for netlify

--- travelyt/svelte.config.js
+++ travelyt/svelte.config.js
@@ -1,11 +1,5 @@
 import adapter from '@sveltejs/adapter-netlify';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
+export default {
 	kit: {
 		adapter: adapter({
 			edge: false,
@@ -13,5 +7,3 @@ const config = {
 		})
 	}
 };
-
-export default config;
